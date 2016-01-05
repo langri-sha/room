fb3000b2d3cc5d9b14655ce3ba75a1494dfb7008
Filip Dupanović <filip.dupanovic@gmail.com>
2016-01-05 23:54:43 +0100

    :crescent_moon: Add run script

---
+++ run.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+#
+# Starts `langrisha/room` in the working directory.
+#
+# See:
+# - https://github.com/langri-sha/room
+
+set -e
+
+# Setup options for connecting to docker host.
+if [ -z "$DOCKER_HOST" ]; then
+    DOCKER_HOST="/var/run/docker.sock"
+fi
+if [ -S "$DOCKER_HOST" ]; then
+    DOCKER_ADDR="-v $DOCKER_HOST:$DOCKER_HOST:rw -e DOCKER_HOST"
+else
+    DOCKER_ADDR="-e DOCKER_HOST -e DOCKER_TLS_VERIFY -e DOCKER_CERT_PATH"
+fi
+
+# Setup volume mounts.
+VOLUMES="-v $(pwd):$(pwd) -v $HOME:$HOME"
+
+exec docker run --rm -ti $DOCKER_ADDR $VOLUMES -w $(pwd) langrisha/room $@
