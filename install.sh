cbbded87fe5c6c5fa87432af512682e502d912cd
Filip Dupanović <filip.dupanovic@gmail.com>
2016-08-23 10:44:59 +0200

    feat(install): Add support for unprivileged execution

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
-
 {
-	curl -L https://raw.githubusercontent.com/langri-sha/room/master/run.sh -o /usr/local/bin/room
-	chmod +x /usr/local/bin/room
+	curl -L https://raw.githubusercontent.com/langri-sha/room/master/run.sh -o /usr/local/bin/room && \
+	chmod +x /usr/local/bin/room && \
+	printf "\nSuccessfullya installed 'room' command.\n\n"
 } || {
-	echo "Failed installing script. See https://github.com/langri-sha/room#room"
+	sudo curl -L https://raw.githubusercontent.com/langri-sha/room/master/run.sh -o /usr/local/bin/room && \
+	sudo chmod +x /usr/local/bin/room && \
+	printf "\nSuccessfully installed 'room' command.\n"
+} || {
+	printf "\nFailed installing room command. See https://github.com/langri-sha/room#room for more options\n"
 }
